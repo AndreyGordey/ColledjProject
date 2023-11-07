070f359f6d86085672d458ebda4c87d557a32ca0
AndreyGordey <multiistomin@gmail.com>
2023-11-07 17:05:47 +0500

    first

---
+++ info/views.py
@@ -0,0 +1,448 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from .models import Dept, Class, Student, Attendance, Course, Teacher, Assign, AttendanceTotal, time_slots, \
+    DAYS_OF_WEEK, AssignTime, AttendanceClass, StudentCourse, Marks, MarksClass
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+# Create your views here.
+'''
+Этот код - это функция index, которая является представлением для обработки запроса на главную страницу. 
+
+@login_required - это декоратор, который указывает, что для доступа к этому представлению пользователь должен быть аутентифицирован (залогинен).
+
+Внутри функции происходит следующее:
+
+1. Проверяется тип пользователя (request.user.is_teacher, request.user.is_student, request.user.is_superuser) и в зависимости от этого возвращается соответствующий шаблон страницы.
+2. Если тип пользователя не определен или не соответствует ни одному из указанных, возвращается шаблон страницы выхода.
+
+Таким образом, функция index определяет, какой шаблон страницы будет отображаться на главной странице в зависимости от типа пользователя.
+'''
+
+@login_required
+def index(request):
+    if request.user.is_teacher:
+        return render(request, 'info/t_homepage.html')
+    if request.user.is_student:
+        return render(request, 'info/homepage.html')
+    if request.user.is_superuser:
+        return render(request, 'info/admin_page.html')
+    return render(request, 'info/logout.html')
+
+'''2. это функция attendance, которая является представлением для обработки запроса на страницу посещаемости студента.'''
+@login_required()
+def attendance(request, stud_id):
+    stud = Student.objects.get(USN=stud_id)
+    ass_list = Assign.objects.filter(class_id_id=stud.class_id)
+    att_list = []
+    for ass in ass_list:
+        try:
+            a = AttendanceTotal.objects.get(student=stud, course=ass.course)
+        except AttendanceTotal.DoesNotExist:
+            a = AttendanceTotal(student=stud, course=ass.course)
+            a.save()
+        att_list.append(a)
+    return render(request, 'info/attendance.html', {'att_list': att_list})
+
+'''3. это функция attendance_detail, которая является представлением для обработки запроса на страницу детальной информации о посещаемости студента.'''
+@login_required()
+def attendance_detail(request, stud_id, course_id):
+    stud = get_object_or_404(Student, USN=stud_id)
+    cr = get_object_or_404(Course, id=course_id)
+    att_list = Attendance.objects.filter(course=cr, student=stud).order_by('date')
+    return render(request, 'info/att_detail.html', {'att_list': att_list, 'cr': cr})
+
+
+# Teacher Views
+'''4. это функция t_clas, которая является представлением для обработки запроса на страницу информации о классе учителя.'''
+@login_required
+def t_clas(request, teacher_id, choice):
+    teacher1 = get_object_or_404(Teacher, id=teacher_id)
+    return render(request, 'info/t_clas.html', {'teacher1': teacher1, 'choice': choice})
+
+'''5. это функция t_student, которая является представлением для обработки запроса на страницу информации о студентах в классе.'''
+@login_required()
+def t_student(request, assign_id):
+    ass = Assign.objects.get(id=assign_id)
+    att_list = []
+    for stud in ass.class_id.student_set.all():
+        try:
+            a = AttendanceTotal.objects.get(student=stud, course=ass.course)
+        except AttendanceTotal.DoesNotExist:
+            a = AttendanceTotal(student=stud, course=ass.course)
+            a.save()
+        att_list.append(a)
+    return render(request, 'info/t_students.html', {'att_list': att_list})
+
+'''6. это функция t_class_date, которая является представлением для обработки запроса на страницу информации о посещаемости класса по определенной дате.'''
+@login_required()
+def t_class_date(request, assign_id):
+    now = timezone.now()
+    ass = get_object_or_404(Assign, id=assign_id)
+    att_list = ass.attendanceclass_set.filter(date__lte=now).order_by('-date')
+    return render(request, 'info/t_class_date.html', {'att_list': att_list})
+
+'''7.'''
+@login_required()
+def cancel_class(request, ass_c_id):
+    assc = get_object_or_404(AttendanceClass, id=ass_c_id)
+    assc.status = 2
+    assc.save()
+    return HttpResponseRedirect(reverse('t_class_date', args=(assc.assign_id,)))
+
+'''8. это функция cancel_class, которая используется для отмены занятия класса.'''
+@login_required()
+def t_attendance(request, ass_c_id):
+    assc = get_object_or_404(AttendanceClass, id=ass_c_id)
+    ass = assc.assign
+    c = ass.class_id
+    context = {
+        'ass': ass,
+        'c': c,
+        'assc': assc,
+    }
+    return render(request, 'info/t_attendance.html', context)
+
+'''9. это функция edit_att, которая используется для редактирования посещаемости класса.'''
+@login_required()
+def edit_att(request, ass_c_id):
+    assc = get_object_or_404(AttendanceClass, id=ass_c_id)
+    cr = assc.assign.course
+    att_list = Attendance.objects.filter(attendanceclass=assc, course=cr)
+    context = {
+        'assc': assc,
+        'att_list': att_list,
+    }
+    return render(request, 'info/t_edit_att.html', context)
+
+'''10. это функция confirm, которая используется для подтверждения и сохранения изменений посещаемости класса.'''
+@login_required()
+def confirm(request, ass_c_id):
+    assc = get_object_or_404(AttendanceClass, id=ass_c_id)
+    ass = assc.assign
+    cr = ass.course
+    cl = ass.class_id
+    for i, s in enumerate(cl.student_set.all()):
+        status = request.POST[s.USN]
+        if status == 'present':
+            status = 'True'
+        else:
+            status = 'False'
+        if assc.status == 1:
+            try:
+                a = Attendance.objects.get(course=cr, student=s, date=assc.date, attendanceclass=assc)
+                a.status = status
+                a.save()
+            except Attendance.DoesNotExist:
+                a = Attendance(course=cr, student=s, status=status, date=assc.date, attendanceclass=assc)
+                a.save()
+        else:
+            a = Attendance(course=cr, student=s, status=status, date=assc.date, attendanceclass=assc)
+            a.save()
+            assc.status = 1
+            assc.save()
+
+    return HttpResponseRedirect(reverse('t_class_date', args=(ass.id,)))
+
+'''11.  это функция t_attendance_detail, которая используется для отображения деталей посещаемости студента.'''
+@login_required()
+def t_attendance_detail(request, stud_id, course_id):
+    stud = get_object_or_404(Student, USN=stud_id)
+    cr = get_object_or_404(Course, id=course_id)
+    att_list = Attendance.objects.filter(course=cr, student=stud).order_by('date')
+    return render(request, 'info/t_att_detail.html', {'att_list': att_list, 'cr': cr})
+
+'''12. это функция change_att, которая используется для изменения статуса посещаемости.'''
+@login_required()
+def change_att(request, att_id):
+    a = get_object_or_404(Attendance, id=att_id)
+    a.status = not a.status
+    a.save()
+    return HttpResponseRedirect(reverse('t_attendance_detail', args=(a.student.USN, a.course_id)))
+
+'''13. это функция t_extra_class, которая используется для отображения дополнительного класса, связанного с определенным заданием.'''
+@login_required()
+def t_extra_class(request, assign_id):
+    ass = get_object_or_404(Assign, id=assign_id)
+    c = ass.class_id
+    context = {
+        'ass': ass,
+        'c': c,
+    }
+    return render(request, 'info/t_extra_class.html', context)
+
+'''14. это функция e_confirm, которая используется для подтверждения посещаемости студентов для определенного задания.'''
+@login_required()
+def e_confirm(request, assign_id):
+    ass = get_object_or_404(Assign, id=assign_id)
+    cr = ass.course
+    cl = ass.class_id
+    assc = ass.attendanceclass_set.create(status=1, date=request.POST['date'])
+    assc.save()
+
+    for i, s in enumerate(cl.student_set.all()):
+        status = request.POST[s.USN]
+        if status == 'present':
+            status = 'True'
+        else:
+            status = 'False'
+        date = request.POST['date']
+        a = Attendance(course=cr, student=s, status=status, date=date, attendanceclass=assc)
+        a.save()
+
+    return HttpResponseRedirect(reverse('t_clas', args=(ass.teacher_id, 1)))
+
+'''15. это функция t_report, которая используется для отображения отчета о студентах, связанных с определенным заданием.'''
+@login_required()
+def t_report(request, assign_id):
+    ass = get_object_or_404(Assign, id=assign_id)
+    sc_list = []
+    for stud in ass.class_id.student_set.all():
+        a = StudentCourse.objects.get(student=stud, course=ass.course)
+        sc_list.append(a)
+    return render(request, 'info/t_report.html', {'sc_list': sc_list})
+
+#  16 16. Этот код - это функция timetable, которая является представлением для обработки запроса на страницу расписания.
+# Внутри функции происходит следующее:
+@login_required()
+def timetable(request, class_id):
+    asst = AssignTime.objects.filter(assign__class_id=class_id)
+    matrix = [['' for i in range(12)] for j in range(6)]
+
+    for i, d in enumerate(DAYS_OF_WEEK):
+        t = 0
+        for j in range(12):
+            if j == 0:
+                matrix[i][0] = d[0]
+                continue
+            if j == 4 or j == 8:
+                continue
+            try:
+                a = asst.get(period=time_slots[t][0], day=d[0])
+                matrix[i][j] = a.assign.course_id
+            except AssignTime.DoesNotExist:
+                pass
+            t += 1
+
+    context = {'matrix': matrix}
+    return render(request, 'info/timetable.html', context)
+
+'''17. '''
+@login_required()
+def t_timetable(request, teacher_id):
+    asst = AssignTime.objects.filter(assign__teacher_id=teacher_id)
+    class_matrix = [[True for i in range(12)] for j in range(6)]
+    for i, d in enumerate(DAYS_OF_WEEK):
+        t = 0
+        for j in range(12):
+            if j == 0:
+                class_matrix[i][0] = d[0]
+                continue
+            if j == 4 or j == 8:
+                continue
+            try:
+                a = asst.get(period=time_slots[t][0], day=d[0])
+                class_matrix[i][j] = a
+            except AssignTime.DoesNotExist:
+                pass
+            t += 1
+
+    context = {
+        'class_matrix': class_matrix,
+    }
+    return render(request, 'info/t_timetable.html', context)
+
+'''18. это функция t_timetable, которая используется для отображения расписания учителя.'''
+@login_required()
+def free_teachers(request, asst_id):
+    asst = get_object_or_404(AssignTime, id=asst_id)
+    ft_list = []
+    t_list = Teacher.objects.filter(assign__class_id__id=asst.assign.class_id_id)
+    for t in t_list:
+        at_list = AssignTime.objects.filter(assign__teacher=t)
+        if not any([True if at.period == asst.period and at.day == asst.day else False for at in at_list]):
+            ft_list.append(t)
+
+    return render(request, 'info/free_teachers.html', {'ft_list': ft_list})
+
+
+# student marks
+
+'''19. это функция marks_list, которая используется для отображения списка оценок студента.'''
+@login_required()
+def marks_list(request, stud_id):
+    stud = Student.objects.get(USN=stud_id, )
+    ass_list = Assign.objects.filter(class_id_id=stud.class_id)
+    sc_list = []
+    for ass in ass_list:
+        try:
+            sc = StudentCourse.objects.get(student=stud, course=ass.course)
+        except StudentCourse.DoesNotExist:
+            sc = StudentCourse(student=stud, course=ass.course)
+            sc.save()
+            sc.marks_set.create(type='I', name='Internal test 1')
+            sc.marks_set.create(type='I', name='Internal test 2')
+            sc.marks_set.create(type='I', name='Internal test 3')
+            sc.marks_set.create(type='E', name='Event 1')
+            sc.marks_set.create(type='E', name='Event 2')
+            sc.marks_set.create(type='S', name='Semester End Exam')
+        sc_list.append(sc)
+
+    return render(request, 'info/marks_list.html', {'sc_list': sc_list})
+
+
+# teacher marks
+
+'''20. это функция t_marks_list, которая используется для отображения списка оценок для определенного задания.'''
+@login_required()
+def t_marks_list(request, assign_id):
+    ass = get_object_or_404(Assign, id=assign_id)
+    m_list = MarksClass.objects.filter(assign=ass)
+    return render(request, 'info/t_marks_list.html', {'m_list': m_list})
+
+'''21. это функция t_marks_entry, которая используется для отображения информации об оценке для определенного задания.'''
+@login_required()
+def t_marks_entry(request, marks_c_id):
+    mc = get_object_or_404(MarksClass, id=marks_c_id)
+    ass = mc.assign
+    c = ass.class_id
+    context = {
+        'ass': ass,
+        'c': c,
+        'mc': mc,
+    }
+    return render(request, 'info/t_marks_entry.html', context)
+
+'''22. это функция marks_confirm, которая используется для подтверждения и сохранения оценок студентов.'''
+@login_required()
+def marks_confirm(request, marks_c_id):
+    mc = get_object_or_404(MarksClass, id=marks_c_id)
+    ass = mc.assign
+    cr = ass.course
+    cl = ass.class_id
+    for s in cl.student_set.all():
+        mark = request.POST[s.USN]
+        sc = StudentCourse.objects.get(course=cr, student=s)
+        m = sc.marks_set.get(name=mc.name)
+        m.marks1 = mark
+        m.save()
+    mc.status = True
+    mc.save()
+
+    return HttpResponseRedirect(reverse('t_marks_list', args=(ass.id,)))
+
+'''23. это функция edit_marks, которая используется для редактирования оценок студентов.'''
+@login_required()
+def edit_marks(request, marks_c_id):
+    mc = get_object_or_404(MarksClass, id=marks_c_id)
+    cr = mc.assign.course
+    stud_list = mc.assign.class_id.student_set.all()
+    m_list = []
+    for stud in stud_list:
+        sc = StudentCourse.objects.get(course=cr, student=stud)
+        m = sc.marks_set.get(name=mc.name)
+        m_list.append(m)
+    context = {
+        'mc': mc,
+        'm_list': m_list,
+    }
+    return render(request, 'info/edit_marks.html', context)
+
+'''24. это функция student_marks, которая используется для отображения оценок студентов.'''
+@login_required()
+def student_marks(request, assign_id):
+    ass = Assign.objects.get(id=assign_id)
+    sc_list = StudentCourse.objects.filter(student__in=ass.class_id.student_set.all(), course=ass.course)
+    return render(request, 'info/t_student_marks.html', {'sc_list': sc_list})
+
+'''25. это функция add_teacher, которая используется для добавления учителя в систему.'''
+@login_required()
+def add_teacher(request):
+    if not request.user.is_superuser:
+        return redirect("/")
+
+    if request.method == 'POST':
+        dept = get_object_or_404(Dept, id=request.POST['dept'])
+        name = request.POST['full_name']
+        id = request.POST['id'].lower()
+        dob = request.POST['dob']
+        sex = request.POST['sex']
+        
+        # Creating a User with teacher username and password format
+        # USERNAME: firstname + underscore + unique ID
+        # PASSWORD: firstname + underscore + year of birth(YYYY)
+        user = User.objects.create_user(
+            username=name.split(" ")[0].lower() + '_' + id,
+            password=name.split(" ")[0].lower() + '_' + dob.replace("-","")[:4]
+        )
+        user.save()
+
+        Teacher(
+            user=user,
+            id=id,
+            dept=dept,
+            name=name,
+            sex=sex,
+            DOB=dob
+        ).save()
+        return redirect('/')
+    
+    all_dept = Dept.objects.order_by('-id')
+    context = {'all_dept': all_dept}
+
+    return render(request, 'info/add_teacher.html', context)
+
+'''26. это функция add_student, которая используется для добавления студента в систему.'''
+@login_required()
+def add_student(request):
+    # If the user is not admin, they will be redirected to home
+    if not request.user.is_superuser:
+        return redirect("/")
+
+    if request.method == 'POST':
+        # Retrieving all the form data that has been inputted
+        class_id = get_object_or_404(Class, id=request.POST['class'])
+        name = request.POST['full_name']
+        usn = request.POST['usn']
+        dob = request.POST['dob']
+        sex = request.POST['sex'] 
+
+        # Creating a User with student username and password format
+        # USERNAME: firstname + underscore + last 3 digits of USN
+        # PASSWORD: firstname + underscore + year of birth(YYYY)
+        user = User.objects.create_user(
+            username=name.split(" ")[0].lower() + '_' + request.POST['usn'][-3:],
+            password=name.split(" ")[0].lower() + '_' + dob.replace("-","")[:4]
+        )
+        user.save()
+
+        # Creating a new student instance with given data and saving it.
+        Student(
+            user=user,
+            USN=usn,
+            class_id=class_id,
+            name=name,
+            sex=sex,
+            DOB=dob
+        ).save()
+        return redirect('/')
+    
+    all_classes = Class.objects.order_by('-id')
+    context = {'all_classes': all_classes}
+    return render(request, 'info/add_student.html', context)
+
+'''16. Этот код - это функция timetable, которая является представлением для обработки запроса на страницу расписания.
+Внутри функции происходит следующее:
+
+1. Получается список объектов AssignTime, отфильтрованных по class_id.
+2. Создается матрица размером 6x12, где каждый элемент изначально пустой строкой.
+3. Происходит заполнение матрицы значениями из объектов AssignTime. Если объект существует для данного времени и дня недели, то соответствующий элемент матрицы заполняется значением a.assign.course_id.
+4. Создается контекст с данными для шаблона страницы.
+5. Возвращается отрендеренный шаблон info/timetable.html с переданным контекстом.
+
+Таким образом, функция timetable формирует и отображает расписание на странице, используя данные из базы данных.'''
